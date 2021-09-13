49fd4fbe2ce82330bf0e4c3dd2e7ae53295ebfe1
Rodrigo Fontes <rodrifontes@gmail.com>
2021-09-13 18:45:29 -0300

    Convertendo Projeto para Styled Component

---
+++ src/Components/ImageFilter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import alimentacao from '../assets/images/alimentacao.svg';
+import outros from '../assets/images/outros.svg';
+import saude from '../assets/images/saude.svg';
+import transporte from '../assets/images/transporte.svg';
+import utilidades from '../assets/images/utilidades.svg';
+import { Icone } from '../Components/UI';
+
+export default (type) => {
+  
+  const Images = {
+    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
+    Saude: <Icone src={saude} alt="Saúde" />,
+    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
+    Transporte: <Icone src={transporte} alt="Transporte" />,
+    default: <Icone src={outros} alt="Outro" />,
+  };
+
+  return Images[type] || Images.default;
+
+};
