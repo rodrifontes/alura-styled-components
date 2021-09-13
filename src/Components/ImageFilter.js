b0d8954809c6030146b09a4ef60837d9d08c7f9a
Rodrigo Fontes <rodrifontes@gmail.com>
2021-09-13 19:18:40 -0300

    Tema

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -5,16 +5,16 @@ import outros from '../assets/images/outros.svg';
 import saude from '../assets/images/saude.svg';
 import transporte from '../assets/images/transporte.svg';
 import utilidades from '../assets/images/utilidades.svg';
-import { Icone } from '../Components/UI';
+import { IconeTema } from '../Components/UI';
 
 export default (type) => {
   
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Saude: <Icone src={saude} alt="Saúde" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    default: <Icone src={outros} alt="Outro" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Saude: <IconeTema src={saude} alt="Saúde" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    default: <IconeTema src={outros} alt="Outro" />,
   };
 
   return Images[type] || Images.default;
